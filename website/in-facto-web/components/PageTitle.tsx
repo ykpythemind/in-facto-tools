2540e7b122dd9721b5de2580d2a9b6ccce59a7f0
ykpythemind <yukibukiyou@gmail.com>
2023-04-13 01:51:33 +0900

    fix

--- website/in-facto-web/components/PageTitle.tsx
+++
@@ -1,3 +0,0 @@
-export const PageTitle = ({ title }: { title: string }) => {
-  return <h2 className="text-2xl font-serif">{title}</h2>;
-};
--- website/in-facto-web/lib/env.ts
+++
@@ -1,4 +0,0 @@
-export const isPreviewEnv = process.env.NEXT_PUBLIC_IS_PREVIEW === "true";
-
-export const isRealProduction =
-  process.env.NEXT_PUBLIC_IS_REAL_PRODUCTION === "true";
